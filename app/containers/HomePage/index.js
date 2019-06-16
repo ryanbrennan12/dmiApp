0a9916c9d85c4aac970c9df5bf61fe3fd1510a0d
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-16 13:15:15 -0700

    refactor entry files

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -4,11 +4,23 @@
  * This is the first thing users see of our App, at the '/' route
  *
  */
-
 import React from 'react';
-// import { FormattedMessage } from 'react-intl';
-// import messages from './messages';
 
-export default function HomePage() {
-  return <h1>Testinggggg</h1>;
-}
+const HomePage = () => (
+  <div className="container">
+    <h4 className="center">homePage baybeee</h4>
+    <p>
+      Glossier keytar chia, tote bag chartreuse everyday carry church-key health
+      goth organic. Single-origin coffee ugh sustainable, authentic hell of
+      heirloom microdosing semiotics VHS raclette jianbing ennui. Irony cray
+      chillwave, wayfarers polaroid ethical slow-carb freegan cronut gentrify
+      jean shorts godard. Enamel pin blog biodiesel cliche sustainable vice
+      shaman chartreuse craft beer helvetica. Taiyaki pour-over VHS drinking
+      vinegar lumbersexual, umami austin mumblecore chicharrones. Fanny pack
+      mustache keffiyeh literally, ugh scenester tousled chambray kombucha
+      butcher cliche intelligentsia. Edison bulb mixtape aesthetic austin
+      poutine.
+    </p>
+  </div>
+);
+export default HomePage;
