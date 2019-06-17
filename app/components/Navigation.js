0d9f69faed4013e29939e49a63175ef0518f2391
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-16 18:01:06 -0700

    [complete] styled component navbar

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavigationBar = styled.div`
+  margin-bottom: 15px;
+  background-color: lightgray;
+`;
+
 
 export default () => (
-  <div>
+   <NavigationBar>
     <Link className="btn btn-primary" to="/">
-      Awesome List
+      Home/List 🏡
     </Link>
     <Link className="btn btn-secondary" to="/new-item">
-      + Add New Thing;-)
+      + Add New Thing 🚀
     </Link>
-  </div>
+    </NavigationBar>
 );
